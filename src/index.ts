00890e7db35088557ecffb31f851fd9b218c99f3
Pistonight <pistonknight@outlook.com>
2025-05-03 17:28:51 -0700

    add InlineLink

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./DarkToggle.tsx";
 export * from "./GitHubLink.tsx";
+export * from "./InlineLink.tsx";
 export * from "./LanguagePicker.tsx";
 export * from "./ResizeLayout.tsx";
 export * from "./ThemeProvider.tsx";
